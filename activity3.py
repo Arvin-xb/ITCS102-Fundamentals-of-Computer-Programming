74e52d22da688b7613f3ef846ceef9b0c2efebaa
Arvin-xb <arvingozoo@gmail.com>
2026-08-23 17:34:46 +0800

    Add files via upload
    ITCS102 THIRD ACTIVITY

---
+++ activity3.py
@@ -0,0 +1,46 @@
+#BSIT
+#LETSGOJOHNNY!!
+
+#str
+fname = "Arvin"
+mname = "D."
+sname = "Gozo"
+nname = "Arvino"
+
+print("First Name:" ,fname)
+print("Middle Initial:",mname)
+print("Surname:",sname)
+print("Nickname:",nname)
+
+#int
+age = 17
+height = 66
+weight = 58
+fav_number = 47
+
+print("Age:",age)
+print("Height(inch):",height)
+print("Weight(kg):",weight)
+print("Favorite Number:",fav_number)
+
+#float
+height = 167.64
+temp_c = 36.5
+bmi = 20.6
+foot_lenght = 25.4
+
+print("Height(cm):",height)
+print("Body Temperature(°C):",temp_c)
+print("Body Mass Index:",bmi)
+print("Foot Length(cm):",foot_lenght)
+
+#bool
+isSingle = True
+isOnlyChild = True
+isFemale = False
+isGood = True
+
+print("Status: Single =",isSingle)
+print("Only Child:",isOnlyChild)
+print("Sex: Female =",isFemale)
+print("Kind: Good =", isGood)
