464dde085f854c07a674cf9dd109602b14d235a9
Arvin <arvingozoo@gmail.com>
2026-08-25 17:06:53 +0800

    Add files via upload
    Activity 4

---
+++ activity4.py
@@ -0,0 +1,12 @@
+#act4
+#escape secquence
+
+#\n - new line
+#\t - tab space
+#\r
+#\b - backspace
+#\\ - to make double quotation
+
+phrase1 = "\r\"Dagat tatawirin \n\t\tmakasama lang kita kahit malalim. \n\t\t\t\t\t\t 'Di makakailang adik\b\b\b\b na nga ako\""
+
+print(phrase1)
