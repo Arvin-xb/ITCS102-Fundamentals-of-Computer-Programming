bd8d288871bfad6568d6c1bb0479838dcd19f68f
Arvin <arvingozoo@gmail.com>
2026-08-26 20:08:48 +0800

    Add files via upload
    #CodeChallenge2
    - Gozo

---
+++ codechallenge2.py
@@ -0,0 +1,17 @@
+#operator = symbol
+#heehee
+
+moneytd = 19863
+fd = 19863 // 1000
+fd2 = 863 // 500
+fd3 = 363 // 200
+fd4 = 163 // 100
+fd5 = 63 // 50
+fd6 = 13 // 20
+fd7 = 13 // 10
+fd8 = 3 // 5
+fd9 = 3 // 1
+
+
+print("Money to Deposit:",moneytd, "\n1000 =",fd, "\n500 =", fd2, "\n200 =",fd3, "\n100 =",fd4, "\n50 =",fd5, "\n20 =",fd6, "\n10 =",fd7, "\n5 =",fd8, "\n1 =",fd9, "\nTotal Money Diposited:",moneytd)
+ 
