3c6cc28dc507fd040da78623335814765c411ea9
Arvin <arvingozoo@gmail.com>
2026-08-28 23:15:47 +0800

    Add files via upload
    #Activity5
    #ArvinLangSaKanal

---
+++ activity5.py
@@ -0,0 +1,28 @@
+#operator = symbol
+# + = add
+# - = subtract
+# / = divide
+# * = multiply
+# // = floor division
+# ** = exponent
+# % = modulo
+
+a = 15
+b = 3
+
+add = a + b
+sub = a - b
+div = a / b
+multi = a * b
+fdiv = a // b
+expo = a ** b
+mod = a % b
+
+
+print("The adddition of number",a,"and",b,":",add)
+print("The subtraction of number",a,"and",b,":",sub)
+print("The division of number",a,"and",b,":", div)
+print("The multiplication of number",a,"and",b,":",multi)
+print("The floor division of number",a,"and",b,":",fdiv)
+print("The exponent of number",a,"and",b,":",expo)
+print("The remainder of number",a,"and",b,":",mod)
