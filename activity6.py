22ecb89dc0a3fa8b5bc87b86ffe9e0bf8bba7b8a
Arvin <arvingozoo@gmail.com>
2026-09-02 20:34:54 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,11 @@
+#Input
+#ACT6
+#LETSGOJOHNNY!!
+
+name = input("Enter Your Name: ")
+
+print("Hello,",name,"!!")
+
+print("How old are you?")
+age = int(input("Enter your age: "))
+print("Okay Bye!!")
