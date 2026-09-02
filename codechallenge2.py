fd92a99d3f1098629252ebf987c2031f6801d320
Arvin <arvingozoo@gmail.com>
2026-09-02 20:27:21 +0800

    Update codechallenge2.py

--- codechallenge2.py
+++ codechallenge2.py
@@ -1,7 +1,8 @@
 #operator = symbol
 #heehee
 
-moneytd = int(input("Enter Money to Deposit:"))
+print("==================PH BANK DENOMINATION=======================")
+moneytd = eval(input("Enter Money to Deposit:"))
 
 num1 = moneytd // 1000
 num2 = moneytd % 1000 // 500
@@ -13,4 +14,4 @@ num7 = moneytd % 1000 % 500 % 200 % 100 % 50 % 20 // 10
 num8 = moneytd % 1000 % 500 % 200 % 100 % 50 % 20 % 10 // 5
 num9 = moneytd % 1000 % 500 % 200 % 100 % 50 % 20 % 10 % 5 // 1
 
-print("\n1000 =",num1,"\n500 =",num2,"\n200 =",num3,"\n100 =",num4,"\n50 =",num5,"\n20 =",num6,"\n10 =",num7,"\n5 =",num8,"\n1 =",num9,"\n\n Total Amount of Money Deposited:",moneytd)
+print("\n\t1000 =",num1,"\n\t500 =",num2,"\n\t200 =",num3,"\n\t100 =",num4,"\n\t50 =",num5,"\n\t20 =",num6,"\n\t10 =",num7,"\n\t5 =",num8,"\n\t1 =",num9,"\n\n Total Amount of Money Deposited:",moneytd)
