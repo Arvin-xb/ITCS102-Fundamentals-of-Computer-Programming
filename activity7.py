13b429dd251c12a79b202d280ef0069fdbb1acdb
Arvin <arvingozoo@gmail.com>
2026-09-04 11:12:16 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,34 @@
+#act 7
+
+
+#type()
+
+#x = eval(input("Enter a Number: "))
+
+#print(type(x)
+
+#total = x + 50
+
+#print("Your Number:",x," + 50 =",total)
+
+
+total = 5
+n = eval(input("Enter a number:"))
+total = total + n
+
+a = eval(input("Enter a number:"))
+total += a
+
+b = eval(input("Enter a number:"))
+total -= b
+
+c = eval(input("Enter a number:"))
+total *= c
+
+d = eval(input("Enter a number:"))
+total /= d
+
+e = eval(input("Enter a number:"))
+total += e
+
+print("Current Total =", total)
