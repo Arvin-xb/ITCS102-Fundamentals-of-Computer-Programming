2be204b9dfb0e440f302784d6de19eea1d0bde4c
Arvin <arvingozoo@gmail.com>
2026-09-07 11:06:27 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,20 @@
+# > - greater than
+# < - less than
+# >= 
+# <=
+# != not equal
+# == equal to
+
+x = 19
+y = 12
+
+
+
+print ( x > y )
+print ( x >= y )
+print ( x <= y )
+print ( x != y)
+print ( x == y)		 	
+
+
+
