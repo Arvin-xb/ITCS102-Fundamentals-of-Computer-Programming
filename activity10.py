f5c27f73e800503b77cf117580d544f4738d7c17
Arvin <arvingozoo@gmail.com>
2026-09-07 11:19:45 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,20 @@
+#CONDITIONAL STATEMENT
+
+username = "arvino"
+password = "heehee"
+
+
+u = input("Enter Username: ")
+p = input("Enter Password: ")
+
+if u == username and p == password:
+   print("Username Correct\nPassword Correct")
+
+else: 
+   print("Access Denied")
+
+
+#LOGICAL OPERATOR
+#and &&
+#or
+#not 
