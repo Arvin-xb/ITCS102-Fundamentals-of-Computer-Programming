3e0194198b743c8631ace0728ff7a25023c68c1d
Arvin <arvingozoo@gmail.com>
2026-09-11 14:13:07 +0800

    Create activity8.py

---
+++ activity8.py
@@ -0,0 +1,14 @@
+#emptystring
+
+games = " "
+
+a = input("GAMES YOU LOVE TO PLAY: ")
+games += a + " ," 
+a = input("ANOTHER: ")
+games += a + " ,"
+a = input("ANOTHER: ")
+games += a + " ,"
+a = input("ANOTHER: ")
+games += a + " ,"
+
+print("YOUR FAVORITE GAMES ARE{",games,"}")
